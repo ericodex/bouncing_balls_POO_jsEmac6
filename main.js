b6ce06c3cd5cc6dca0073117ef5fc891ac0c548a
Ericódigos <eric.075315@gmail.com>
2019-07-10 14:05:47 -0300

    Add CSS, JS

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 /*jshint esversion: 6 */
-import * as ldsh from lodash;
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
 
