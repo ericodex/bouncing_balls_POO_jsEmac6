4c1ea5aa6ebb69b19d13576dce841fe6402f3bc9
Ericódigos <eric.075315@gmail.com>
2019-07-10 23:47:11 -0300

    Adicionado método de colisão

--- main.js
+++ main.js
@@ -63,23 +63,56 @@ Bolinha.prototype.atualizar = function(){
 // Animando a Bolinha
 var lista_bolas = [];
 
-while (lista_bolas.length < 25) {
-  var tamanho = random(10,20);
-  var bolinha = new Bolinha(
-    /** Novas bolinhas são posicionadas a pelo menos uma bolinha de distância do 
-     * limite do canvas/tela para evitar erros de desenho.
-     */
-    random(0 + tamanho,width - tamanho),
-    random(0 + tamanho, height - tamanho),
-    random(-7,7),
-    random(-7,7),
-    'rgb('+ random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
-    tamanho
-  ); 
-  lista_bolas.push(bolinha);
+
+function encher_lista_bolinhas(){
+  while (lista_bolas.length < 25) {
+    var tamanho = random(10,20);
+    var bolinha = new Bolinha(
+      /** Novas bolinhas são posicionadas a pelo menos uma bolinha de distância do 
+       * limite do canvas/tela para evitar erros de desenho.
+       */
+      random(0 + tamanho,width - tamanho),
+      random(0 + tamanho, height - tamanho),
+      random(-7,7),
+      random(-7,7),
+      'rgb('+ random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
+      tamanho
+    ); 
+    lista_bolas.push(bolinha);
+  }
+}
+
+encher_lista_bolinhas();
+
+function ciclo(){ // Loop de desenho das bolinhas na tela.
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0,0,width,height);
+
+  for(let i = 0; i < lista_bolas.length; i++){
+    lista_bolas[i].desenhar();
+    lista_bolas[i].atualizar();
+    lista_bolas[i].deteccaoColisao();
+  }
+
+  requestAnimationFrame(ciclo);
 }
 
 
+// Adicionando Detector de Colisão 
+Bolinha.prototype.deteccaoColisao = function(){
+  for(let j=0;j<lista_bolas.length;j++){
+    if(!(this===lista_bolas[j])){
+      var dx = this.x - lista_bolas[j].x;
+      var dy = this.y - lista_bolas[j].y;
+      var distância = Math.sqrt(dx*dx+dy*dy);
+
+      if(distância < this.tamanho + lista_bolas[j].tamanho){
+        lista_bolas[j].cor = this.cor = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+};
 
 
+ciclo();
 
